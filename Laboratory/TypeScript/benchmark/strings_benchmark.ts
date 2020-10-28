64b20918126c72aa3d1ca21c8f3c70ee4d607431
Lynn <lynn@foldr.moe>
2020-10-28 22:01:27 +0100

    Add encode benchmarks

--- Laboratory/TypeScript/benchmark/strings_benchmark.ts
+++ Laboratory/TypeScript/benchmark/strings_benchmark.ts
@@ -3,11 +3,12 @@ import benchmark = require('benchmark');
 import { BebopView } from '../test/generated/BebopView';
 const suite = new benchmark.Suite();
 
-for (const length of [10000, 1000, 360, 180, 100, 10]) {
+for (const length of [100000, 10000, 1000, 100, 20]) {
     const m = { strings: ['ab', 'cd', 'ef', 'gh', 'ij'].map(x => x.repeat(length/2)) };
     const bm = ArrayOfStrings.encode(m);
-    console.log([length, bm.length]);
     const jm = JSON.stringify(m);
+    ArrayOfStrings.decode(bm).strings[4][0]; // as a test
+
     suite.add(`Bebop decode ${length} byte string (manual)`, function () {
         BebopView.getInstance().minimumTextDecoderLength = 999999999; // never use it
         ArrayOfStrings.decode(bm).strings[4][0];
@@ -17,6 +18,16 @@ for (const length of [10000, 1000, 360, 180, 100, 10]) {
         ArrayOfStrings.decode(bm).strings[4][0];
     })
     suite.add(`JSON decode ${length} byte string`, function () { JSON.parse(jm).strings[4][0]; })
+
+    suite.add(`Bebop encode ${length} byte string (manual)`, function () {
+        BebopView.getInstance().minimumTextEncoderLength = 999999999; // never use it
+        ArrayOfStrings.encode(m);
+    })
+    suite.add(`Bebop encode ${length} byte string (TextEncoder)`, function () {
+        BebopView.getInstance().minimumTextEncoderLength = 0; // always use it
+        ArrayOfStrings.encode(m);
+    })
+    suite.add(`JSON stringify ${length} byte string`, function () { JSON.stringify(m); })
 }
 
 suite.on('cycle', cycle)
