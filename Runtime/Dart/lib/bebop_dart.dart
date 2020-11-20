6815e0dbaff8250109c861bf6f64d97fb7633b27
Lynn <lynn@foldr.moe>
2020-11-20 01:51:20 +0100

    Dart runtime

---
+++ Runtime/Dart/lib/bebop_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library bebop_dart;
+
+export 'src/bebop.dart';
+
+// TODO: Export any libraries intended for clients of this package.
