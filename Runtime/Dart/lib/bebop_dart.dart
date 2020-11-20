7eace64f7c4af1e88c823b3c4544c56991764e47
Lynn <lynn@foldr.moe>
2020-11-20 01:55:37 +0100

    Doc

--- Runtime/Dart/lib/bebop_dart.dart
+++ Runtime/Dart/lib/bebop_dart.dart
@@ -1,8 +1,9 @@
-/// Support for doing something awesome.
+/// This library provides the classes that the `bebop` Dart codegen output relies on.
 ///
-/// More dartdocs go here.
+/// You shouldn't need to access these classes directly to use Bebop. Instead, add
+/// this package as a dependency and then use the `encode` and `decode` methods in
+/// the code that the Bebop compiler generates for your schemas.
 library bebop_dart;
 
 export 'src/bebop.dart';
 
-// TODO: Export any libraries intended for clients of this package.
