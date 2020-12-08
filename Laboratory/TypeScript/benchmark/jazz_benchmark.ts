84e45a457cafe9e6566b1ee3fd435725f5ea7f4d
Lynn <lynn@foldr.moe>
2020-12-09 00:33:28 +0100

    Benchmark "Song" against Protocol Buffers

--- Laboratory/TypeScript/benchmark/jazz_benchmark.ts
+++ Laboratory/TypeScript/benchmark/jazz_benchmark.ts
@@ -1,7 +1,13 @@
 import { ILibrary, Instrument, Library, ISong, Song, Musician } from '../test/generated/jazz';
 import * as assert from "assert";
 import benchmark = require('benchmark');
-import { BebopView } from '../test/generated/BebopView';
+import msgpack = require("@msgpack/msgpack");
+import Pbf = require('pbf');
+
+// This file was generated with `pbf ./jazz.proto > ./jazz_protobuf.gen.js`,
+// which is how `pbf` (the ProtoBuf implementation benchmarked here) works.
+var PbfSong = require('./jazz_protobuf.gen').Song;
+
 const suite = new benchmark.Suite();
 
 const song1: ISong = {
@@ -28,26 +34,23 @@ const library: ILibrary = {
     ]),
 }
 
-const bebopEncodedSong1 = Song.encode(song1);
-const jsonEncodedSong1 = JSON.stringify(song1);
+const bebopEncodedSong = Song.encode(song1);
+const jsonEncodedSong = JSON.stringify(song1);
+const msgpackEncodedSong = msgpack.encode(song1);
+var pbf = new Pbf();
+PbfSong.write(song1, pbf);
+const protobufEncodedSong = pbf.finish();
 
 /////////////////////////////////////
 
-suite.add('Bebop-encode Song', function () {
-    Song.encode(song1);
-})
-
-suite.add('JSON.stringify Song', function () {
-    JSON.stringify(song1);
-})
-
-suite.add('Bebop-decode Song', function () {
-    Song.decode(bebopEncodedSong1);
-})
-
-suite.add('JSON.parse Song', function () {
-    JSON.parse(jsonEncodedSong1);
-})
+suite.add('Bebop   encode Song', () => Song.encode(song1));
+suite.add('Bebop   decode Song', () => Song.decode(bebopEncodedSong));
+suite.add('JSON    encode Song', () => JSON.stringify(song1));
+suite.add('JSON    decode Song', () => JSON.parse(jsonEncodedSong));
+suite.add('msgpack encode Song', () => msgpack.encode(song1));
+suite.add('msgpack decode Song', () => msgpack.decode(msgpackEncodedSong));
+suite.add('pbf     encode Song', () => { var p = new Pbf(); PbfSong.write(song1, p); p.finish(); });
+suite.add('pbf     decode Song', () => PbfSong.read(new Pbf(protobufEncodedSong)));
 
 suite.on('cycle', cycle)
 
