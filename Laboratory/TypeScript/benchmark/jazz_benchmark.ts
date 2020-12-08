0ab95ae7d68e8bfdba5b449edf6bbc5478bfcb83
Lynn <lynn@foldr.moe>
2020-12-09 00:37:50 +0100

    Clean up the benchmark file

--- Laboratory/TypeScript/benchmark/jazz_benchmark.ts
+++ Laboratory/TypeScript/benchmark/jazz_benchmark.ts
@@ -1,5 +1,4 @@
 import { ILibrary, Instrument, Library, ISong, Song, Musician } from '../test/generated/jazz';
-import * as assert from "assert";
 import benchmark = require('benchmark');
 import msgpack = require("@msgpack/msgpack");
 import Pbf = require('pbf');
@@ -8,8 +7,7 @@ import Pbf = require('pbf');
 // which is how `pbf` (the ProtoBuf implementation benchmarked here) works.
 var PbfSong = require('./jazz_protobuf.gen').Song;
 
-const suite = new benchmark.Suite();
-
+// I'd benchmark "Library", but it contains a Map(), which causes problems.
 const song1: ISong = {
     title: 'Donna Lee',
     year: 1947,
@@ -19,29 +17,14 @@ const song1: ISong = {
     ],
 };
 
-const song2: ISong = {
-    title: 'A Night In Tunisia',
-    year: 1946,
-    performers: [
-        { name: 'Dizzy Gillespie', plays: Instrument.Trumpet },
-    ],
-};
-
-const library: ILibrary = {
-    songs: new Map([
-        ['4f40c472-2eca-4375-b9c6-f8aa87684579', song1],
-        ['2dd446ef-85b4-40fd-91d1-40dbfb93d8c7', song2],
-    ]),
-}
-
 const bebopEncodedSong = Song.encode(song1);
 const jsonEncodedSong = JSON.stringify(song1);
 const msgpackEncodedSong = msgpack.encode(song1);
-var pbf = new Pbf();
-PbfSong.write(song1, pbf);
-const protobufEncodedSong = pbf.finish();
+var modelPbf = new Pbf();
+PbfSong.write(song1, modelPbf);
+const protobufEncodedSong = modelPbf.finish();
 
-/////////////////////////////////////
+const suite = new benchmark.Suite();
 
 suite.add('Bebop   encode Song', () => Song.encode(song1));
 suite.add('Bebop   decode Song', () => Song.decode(bebopEncodedSong));
@@ -52,10 +35,5 @@ suite.add('msgpack decode Song', () => msgpack.decode(msgpackEncodedSong));
 suite.add('pbf     encode Song', () => { var p = new Pbf(); PbfSong.write(song1, p); p.finish(); });
 suite.add('pbf     decode Song', () => PbfSong.read(new Pbf(protobufEncodedSong)));
 
-suite.on('cycle', cycle)
-
-suite.run()
-
-function cycle (e) {
-  console.log(e.target.toString())
-}
+suite.on('cycle', (e) => console.log(e.target.toString()));
+suite.run();
