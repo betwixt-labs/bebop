0b565156971965a84527af78557a0bfc3e051f4e
Lynn <lynn@foldr.moe>
2020-12-09 01:58:17 +0100

    Benchmark TypeScript against Protocol Buffers (#43)
    * Benchmark "Song" against Protocol Buffers
    
    * Clean up the benchmark file

--- Laboratory/TypeScript/benchmark/jazz_benchmark.ts
+++ Laboratory/TypeScript/benchmark/jazz_benchmark.ts
@@ -1,9 +1,13 @@
 import { ILibrary, Instrument, Library, ISong, Song, Musician } from '../test/generated/jazz';
-import * as assert from "assert";
 import benchmark = require('benchmark');
-import { BebopView } from '../test/generated/BebopView';
-const suite = new benchmark.Suite();
+import msgpack = require("@msgpack/msgpack");
+import Pbf = require('pbf');
 
+// This file was generated with `pbf ./jazz.proto > ./jazz_protobuf.gen.js`,
+// which is how `pbf` (the ProtoBuf implementation benchmarked here) works.
+var PbfSong = require('./jazz_protobuf.gen').Song;
+
+// I'd benchmark "Library", but it contains a Map(), which causes problems.
 const song1: ISong = {
     title: 'Donna Lee',
     year: 1947,
@@ -13,46 +17,23 @@ const song1: ISong = {
     ],
 };
 
-const song2: ISong = {
-    title: 'A Night In Tunisia',
-    year: 1946,
-    performers: [
-        { name: 'Dizzy Gillespie', plays: Instrument.Trumpet },
-    ],
-};
+const bebopEncodedSong = Song.encode(song1);
+const jsonEncodedSong = JSON.stringify(song1);
+const msgpackEncodedSong = msgpack.encode(song1);
+var modelPbf = new Pbf();
+PbfSong.write(song1, modelPbf);
+const protobufEncodedSong = modelPbf.finish();
 
-const library: ILibrary = {
-    songs: new Map([
-        ['4f40c472-2eca-4375-b9c6-f8aa87684579', song1],
-        ['2dd446ef-85b4-40fd-91d1-40dbfb93d8c7', song2],
-    ]),
-}
+const suite = new benchmark.Suite();
 
-const bebopEncodedSong1 = Song.encode(song1);
-const jsonEncodedSong1 = JSON.stringify(song1);
+suite.add('Bebop   encode Song', () => Song.encode(song1));
+suite.add('Bebop   decode Song', () => Song.decode(bebopEncodedSong));
+suite.add('JSON    encode Song', () => JSON.stringify(song1));
+suite.add('JSON    decode Song', () => JSON.parse(jsonEncodedSong));
+suite.add('msgpack encode Song', () => msgpack.encode(song1));
+suite.add('msgpack decode Song', () => msgpack.decode(msgpackEncodedSong));
+suite.add('pbf     encode Song', () => { var p = new Pbf(); PbfSong.write(song1, p); p.finish(); });
+suite.add('pbf     decode Song', () => PbfSong.read(new Pbf(protobufEncodedSong)));
 
-/////////////////////////////////////
-
-suite.add('Bebop-encode Song', function () {
-    Song.encode(song1);
-})
-
-suite.add('JSON.stringify Song', function () {
-    JSON.stringify(song1);
-})
-
-suite.add('Bebop-decode Song', function () {
-    Song.decode(bebopEncodedSong1);
-})
-
-suite.add('JSON.parse Song', function () {
-    JSON.parse(jsonEncodedSong1);
-})
-
-suite.on('cycle', cycle)
-
-suite.run()
-
-function cycle (e) {
-  console.log(e.target.toString())
-}
+suite.on('cycle', (e) => console.log(e.target.toString()));
+suite.run();
