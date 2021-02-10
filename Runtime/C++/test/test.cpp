35f37081b48ed01dae118026a086e6c6b75ed079
Lynn <lynn@foldr.moe>
2021-02-10 20:46:01 +0100

    Test floats in runtime test

--- Runtime/C++/test/test.cpp
+++ Runtime/C++/test/test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "../src/bebop.hpp"
+#include <cmath>
 #include <chrono>
 #include <cstddef>
 #include <cstdint>
@@ -10,6 +11,7 @@ int main() {
     const std::string myGuid = "04328465-4290-4bf2-896b-5d05a9084e9b";
     w.writeGuid(bebop::Guid::fromString(myGuid));
     w.writeInt64(12345);
+    w.writeFloat32(12.345);
     w.writeByte(255);
     w.writeDate(bebop::TickDuration(123456789));
     size_t p = w.reserveMessageLength();
@@ -20,6 +22,7 @@ int main() {
     bebop::BebopReader r { buffer.data() };
     std::cout << "guid roundtrip: " << (r.readGuid().toString() == myGuid ? "ok" : "fail") << std::endl;
     std::cout << "int roundtrip: " << (r.readInt64() == 12345 ? "ok" : "fail") << std::endl;
+    std::cout << "float roundtrip: " << (fabs(r.readFloat32() - 12.345) < 0.000001 ? "ok" : "fail") << std::endl;
     std::cout << "byte roundtrip: " << (r.readByte() == 255 ? "ok" : "fail") << std::endl;
     std::cout << "date roundtrip: " << (r.readDate().count() == 123456789 ? "ok" : "fail") << std::endl;
 
