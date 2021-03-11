54da12a7dc0800df132c90d313f820617bb0fe51
Andrew Sampson <andrew@rainway.com>
2021-03-11 04:47:50 -0800

    Various changes
    - Removes "record" types as they are incompatible with the upcoming base class change.
    - Adds a copy constructor.
    - Adds a Deconstruct
    - Adds GetByteCount and GetMaxByteCount generation.

--- Core/Meta/Type.cs
+++ Core/Meta/Type.cs
@@ -1,4 +1,5 @@
-﻿using Core.Lexer.Tokenization.Models;
+﻿using System;
+using Core.Lexer.Tokenization.Models;
 using Core.Meta.Interfaces;
 
 namespace Core.Meta
@@ -68,6 +69,79 @@ namespace Core.Meta
         public DefinedType(string name, Span span, string asString) : base(span, asString)
         {
             Name = name;
+        }
+    }
+
+    public static class TypeExtensions
+    {
+        public static bool IsFixedScalar(this TypeBase type)
+        {
+            return type is ScalarType and { BaseType: not BaseType.String };
+        }
+        public static bool IsStruct(this TypeBase type, ISchema schema)
+        {
+            return type switch
+            {
+                DefinedType dt when schema.Definitions[dt.Name] is StructDefinition => true,
+                _ => false
+            };
+        }
+
+        public static bool IsMessage(this TypeBase type, ISchema schema)
+        {
+            return type switch
+            {
+                DefinedType dt when schema.Definitions[dt.Name] is MessageDefinition => true,
+                _ => false
+            };
+        }
+
+        public static bool IsUnion(this TypeBase type, ISchema schema)
+        {
+            return type switch
+            {
+                DefinedType dt when schema.Definitions[dt.Name] is UnionDefinition => true,
+                _ => false
+            };
+        }
+
+        public static bool IsEnum(this TypeBase type, ISchema schema)
+        {
+            return type switch
+            {
+                DefinedType dt when schema.Definitions[dt.Name] is EnumDefinition => true,
+                _ => false
+            };
+        }
+
+        public static int MinimalEncodedSize(this TypeBase type, ISchema schema)
+        {
+            return type switch
+            {
+                ArrayType or MapType => 4,
+                ScalarType st => st.BaseType.Size(),
+                DefinedType dt when schema is not null && schema.Definitions[dt.Name] is EnumDefinition => 4,
+                DefinedType dt when schema is not null && schema.Definitions[dt.Name] is TopLevelDefinition td => td.MinimalEncodedSize(schema),
+                _ => throw new ArgumentOutOfRangeException(type.ToString())
+            };
+        }
+
+        public static int Size(this BaseType type)
+        {
+            return type switch
+            {
+                BaseType.Bool => 1,
+                BaseType.Byte => 1,
+                BaseType.UInt16 or BaseType.Int16 => 2,
+                BaseType.UInt32 or BaseType.Int32 => 4,
+                BaseType.UInt64 or BaseType.Int64 => 8,
+                BaseType.Float32 => 4,
+                BaseType.Float64 => 8,
+                BaseType.String => 4,
+                BaseType.Guid => 16,
+                BaseType.Date => 8,
+                _ => throw new ArgumentOutOfRangeException(type.ToString()),
+            };
         }
     }
 }
