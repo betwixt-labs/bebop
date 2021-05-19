39578a7d9313fb20a3b508e09df2dd623a655bd6
Lynn <lynn@foldr.moe>
2021-05-19 21:01:44 +0200

    Dart test

--- Core/Meta/VersionInfo.cs
+++ Core/Meta/VersionInfo.cs
@@ -32,6 +32,6 @@ namespace Core.Meta
         /// <summary>
         /// The human readable version which may contain additional labels.
         /// </summary>
-        public const string Informational = "0.0.1-20210519-1850";
+        public const string Informational = "0.0.1-20210519-1901";
     }
 }
