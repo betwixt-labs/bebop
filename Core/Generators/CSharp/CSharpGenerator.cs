faacad2ba657857f63b408e34d9f7ec696255fea
Lynn <lynn@foldr.moe>
2021-05-26 17:34:53 +0200

    use const instead of static readonly when possible

--- Core/Generators/CSharp/CSharpGenerator.cs
+++ Core/Generators/CSharp/CSharpGenerator.cs
@@ -212,7 +212,8 @@ namespace Core.Generators.CSharp
                 }
                 else if (definition is ConstDefinition cd)
                 {
-                    builder.AppendLine($"public static readonly {TypeName(cd.Value.Type)} {cd.Name} = {EmitLiteral(cd.Value)};");
+                    var constish = CanBeDeclaredConst(cd.Value.Type) ? "const" : "static readonly";
+                    builder.AppendLine($"public {constish} {TypeName(cd.Value.Type)} {cd.Name} = {EmitLiteral(cd.Value)};");
                     builder.AppendLine("");
                 }
                 else
@@ -253,6 +254,14 @@ namespace Core.Generators.CSharp
                 _ => throw new ArgumentOutOfRangeException(literal.ToString()),
             };
         }
+
+        private bool CanBeDeclaredConst(TypeBase type) =>
+            type switch
+            {
+                ScalarType st when st.IsFloat || st.IsInteger => true,
+                ScalarType { BaseType: BaseType.Bool or BaseType.String } => true,
+                _ => false,
+            };
 
         #endregion
 
