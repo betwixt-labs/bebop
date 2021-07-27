7a3a5a0fd14277dc9932c1ae6bf80db3ce58df37
Matthew Conover <matthew@rainway.com>
2021-07-27 13:17:57 -0700

    Union p2 (serialization)

--- Laboratory/Rust/GeneratedTestCode/output.g.rs
+++ Laboratory/Rust/GeneratedTestCode/output.g.rs
@@ -3,7 +3,7 @@
 //!
 //!
 //!   bebopc version:
-//!       0.0.1-20210727-1832
+//!       0.0.1-20210727-2017
 //!
 //!
 //!   bebopc source:
@@ -14,7 +14,86 @@
 //! the code is regenerated.
 //!
 
-use bebop::{Record as _, SubRecord as _};
+use std::io::Write as _;
+
+#[derive(Clone, Debug, PartialEq, Default)]
+pub struct InnerM {
+    /// Field 1
+    pub x: core::option::Option<i32>,
+}
+
+impl<'raw> bebop::SubRecord<'raw> for InnerM {
+    const MIN_SERIALIZED_SIZE: usize = bebop::LEN_SIZE + 1;
+    fn serialize<W: std::io::Write>(&self, dest: &mut W) -> bebop::SeResult<usize> {
+        let mut buf = std::vec::Vec::new();
+        if let Some(ref v) = self.x {
+            buf.push(1);
+            v.serialize(&mut buf)?;
+        }
+        buf.push(0);
+        bebop::write_len(dest, buf.len())?;
+        dest.write_all(&buf)?;
+        Ok(buf.len() + bebop::LEN_SIZE)
+    }
+
+    fn deserialize_chained(raw: &'raw [u8]) -> bebop::DeResult<(usize, Self)> {
+        let len = bebop::read_len(raw)?;
+        #[cfg(not(feature = "unchecked"))]
+        if len == 0 {
+            return Err(bebop::DeserializeError::CorruptFrame);
+        }
+
+        if raw.len() < len + bebop::LEN_SIZE {
+            return Err(bebop::DeserializeError::MoreDataExpected(len + bebop::LEN_SIZE - raw.len()));
+        }
+        let mut i = bebop::LEN_SIZE;
+        let mut de = Self::default();
+
+        #[cfg(not(feature = "unchecked"))]
+        let mut last = 0;
+
+        while i < len + bebop::LEN_SIZE {
+            let di = raw[i];
+
+            #[cfg(not(feature = "unchecked"))]
+            if di != 0 {
+                if di < last {
+                    return Err(bebop::DeserializeError::CorruptFrame);
+                }
+                last = di;
+            }
+
+            i += 1;
+            match di {
+                0 => {
+                    break;
+                }
+                1 => {
+                    #[cfg(not(feature = "unchecked"))]
+                    if de.x.is_some() {
+                        return Err(bebop::DeserializeError::DuplicateMessageField);
+                    }
+                    let (read, value) = <i32>::deserialize_chained(&raw[i..])?;
+                    i += read;
+                    de.x = Some(value)
+                }
+                _ => {
+                    i = len + bebop::LEN_SIZE;
+                    break;
+                }
+            }
+        }
+        if i != len + bebop::LEN_SIZE {
+            debug_assert!(i > len + bebop::LEN_SIZE);
+            Err(bebop::DeserializeError::CorruptFrame)
+        }
+        else {
+            Ok((i, de))
+        }
+    }
+}
+
+impl<'raw> bebop::Record<'raw> for InnerM {}
 
 #[derive(Clone, Debug, PartialEq, Default)]
 pub struct A {
@@ -25,7 +104,7 @@ pub struct A {
 impl<'raw> bebop::SubRecord<'raw> for A {
     const MIN_SERIALIZED_SIZE: usize = bebop::LEN_SIZE + 1;
     fn serialize<W: std::io::Write>(&self, dest: &mut W) -> bebop::SeResult<usize> {
-        let mut buf = Vec::new();
+        let mut buf = std::vec::Vec::new();
         if let Some(ref v) = self.b {
             buf.push(1);
             v.serialize(&mut buf)?;
@@ -43,7 +122,7 @@ impl<'raw> bebop::SubRecord<'raw> for A {
             return Err(bebop::DeserializeError::CorruptFrame);
         }
 
-        if raw.len() < len + bebpp::LEN_SIZE {
+        if raw.len() < len + bebop::LEN_SIZE {
             return Err(bebop::DeserializeError::MoreDataExpected(len + bebop::LEN_SIZE - raw.len()));
         }
         let mut i = bebop::LEN_SIZE;
@@ -154,6 +233,39 @@ impl<'raw> bebop::SubRecord<'raw> for C {
 
 impl<'raw> bebop::Record<'raw> for C {}
 
+#[derive(Clone, Debug, PartialEq)]
+pub struct D {
+    pub msg: InnerM,
+}
+
+impl<'raw> bebop::SubRecord<'raw> for D {
+    const MIN_SERIALIZED_SIZE: usize =
+        <InnerM>::MIN_SERIALIZED_SIZE;
+
+    fn serialize<W: std::io::Write>(&self, dest: &mut W) -> bebop::SeResult<usize> {
+        Ok(
+            self.msg.serialize(dest)?
+        )
+    }
+
+    fn deserialize_chained(raw: &'raw [u8]) -> bebop::DeResult<(usize, Self)> {
+        if raw.len() < Self::MIN_SERIALIZED_SIZE {
+            let missing = raw.len() - Self::MIN_SERIALIZED_SIZE;
+            return Err(bebop::DeserializeError::MoreDataExpected(missing));
+        }
+
+        let mut i = 0;
+        let (read, v0) = <InnerM>::deserialize_chained(raw)?;
+        i += read;
+
+        Ok((i, Self {
+            msg: v0,
+        }))
+    }
+}
+
+impl<'raw> bebop::Record<'raw> for D {}
+
 /// This union is so documented!
 #[derive(Clone, Debug, PartialEq)]
 pub enum U {
@@ -175,6 +287,65 @@ pub enum U {
     /// Discriminator 3
     C {
     },
+
+    /// Discriminator 4
+    D {
+        msg: InnerM,
+    },
+}
+
+impl<'raw> bebop::SubRecord<'raw> for U {
+    const MIN_SERIALIZED_SIZE: usize = bebop::LEN_SIZE + 1;
+
+    fn serialize<W: std::io::Write>(&self, dest: &mut W) -> bebop::SeResult<usize> {
+        let mut buf = std::vec::Vec::new();
+        match self {
+            U::Unknown => {
+                return Err(bebop::SerializeError::CannotSerializeUnknownUnion);
+            }
+            U::A {
+                b: ref _b,
+            }
+            => {
+                buf.push(1);
+                let mut msgbuf = std::vec::Vec::new();
+                if let Some(ref v) = _b {
+                    msgbuf.push(1);
+                    v.serialize(&mut msgbuf)?;
+                }
+                msgbuf.push(0);
+                bebop::write_len(&mut buf, msgbuf.len())?;
+                &mut buf.write_all(&msgbuf)?;
+            }
+            U::B {
+                c: ref _c,
+            }
+            => {
+                buf.push(2);
+                _c.serialize(&mut buf);
+            }
+            U::C {
+            }
+            => {
+                buf.push(3);
+            }
+            U::D {
+                msg: ref _msg,
+            }
+            => {
+                buf.push(4);
+                _msg.serialize(&mut buf);
+            }
+        }
+        bebop::write_len(dest, buf.len());
+        dest.write_all(&buf)?;
+        Ok(buf.len() + bebop::LEN_SIZE)
+    }
+
+    fn deserialize_chained(raw: &'raw [u8]) -> bebop::DeResult<(usize, Self)> {
+        todo!()
+    }
+
 }
 
 
@@ -278,6 +449,44 @@ pub enum WeirdOrder {
     /// Discriminator 4
     ThreeIsSkipped {
     },
+}
+
+impl<'raw> bebop::SubRecord<'raw> for WeirdOrder {
+    const MIN_SERIALIZED_SIZE: usize = bebop::LEN_SIZE + 1;
+
+    fn serialize<W: std::io::Write>(&self, dest: &mut W) -> bebop::SeResult<usize> {
+        let mut buf = std::vec::Vec::new();
+        match self {
+            WeirdOrder::Unknown => {
+                return Err(bebop::SerializeError::CannotSerializeUnknownUnion);
+            }
+            WeirdOrder::OneComesLast {
+            }
+            => {
+                buf.push(1);
+            }
+            WeirdOrder::TwoComesFirst {
+                b: ref _b,
+            }
+            => {
+                buf.push(2);
+                _b.serialize(&mut buf);
+            }
+            WeirdOrder::ThreeIsSkipped {
+            }
+            => {
+                buf.push(4);
+            }
+        }
+        bebop::write_len(dest, buf.len());
+        dest.write_all(&buf)?;
+        Ok(buf.len() + bebop::LEN_SIZE)
+    }
+
+    fn deserialize_chained(raw: &'raw [u8]) -> bebop::DeResult<(usize, Self)> {
+        todo!()
+    }
+
 }
 
 
