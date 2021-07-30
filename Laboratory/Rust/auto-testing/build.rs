e984e9beed4136cca411d39a6d332464a351b020
Matthew Conover <matthew@rainway.com>
2021-07-30 14:55:27 -0700

    Cargo integration

--- Laboratory/Rust/auto-testing/build.rs
+++ Laboratory/Rust/auto-testing/build.rs
@@ -1,9 +1,5 @@
-use std::collections::LinkedList;
-use std::fs;
-use std::path::{Path, PathBuf};
-use std::process::Command;
-
-const SCHEMA_DIR: &str = "../../Schemas";
+use bebop::cargo as bebop;
+use std::path::PathBuf;
 
 #[cfg(windows)]
 const BEBOP_BIN: &str = "../../../bin/compiler/Windows-Debug/bebopc.exe";
@@ -11,78 +7,9 @@ const BEBOP_BIN: &str = "../../../bin/compiler/Windows-Debug/bebopc.exe";
 const BEBOP_BIN: &str = "../../../../bin/compiler/Linux-Debug/bebopc";
 
 fn main() {
-    println!("cargo:rerun-if-changed={}", BEBOP_BIN);
-    println!("cargo:rerun-if-changed={}", "src/lib.rs");
-    // clean all previously built files
-    fs::read_dir("src")
-        .unwrap()
-        .filter_map(|entry| {
-            let entry = entry.unwrap();
-            let name = entry.file_name().to_str().unwrap().to_string();
-            if entry.file_type().unwrap().is_file()
-                && name.starts_with("_")
-                && name.ends_with(".rs")
-            {
-                Some(name)
-            } else {
-                None
-            }
-        })
-        .for_each(|file| fs::remove_file(format!("src/{}", file)).unwrap());
-
-    // build all files and update lib.rs
-    let files = process_schema_dir(SCHEMA_DIR);
-
-    // update the lib file
-    fs::write(
-        "./src/lib.rs",
-        &files
-            .into_iter()
-            .map(|mut schema_name| {
-                schema_name.insert_str(0, "pub mod ");
-                schema_name.push(';');
-                schema_name.push('\n');
-                schema_name
-            })
-            .collect::<String>(),
-    )
-    .unwrap();
-}
-
-fn process_schema_dir(dir: impl AsRef<Path>) -> LinkedList<String> {
-    let mut list = LinkedList::new();
-    for dir_entry in fs::read_dir(dir.as_ref()).unwrap() {
-        let dir_entry = dir_entry.unwrap();
-        let file_type = dir_entry.file_type().unwrap();
-        let path = PathBuf::from(dir.as_ref()).join(dir_entry.file_name());
-        if file_type.is_dir() {
-            if dir_entry.file_name() == "ShouldFail" {
-                // do nothing
-            } else {
-                list.append(&mut process_schema_dir(path));
-            }
-        } else if file_type.is_file() {
-            let file_stem = dir_entry
-                .path()
-                .file_stem()
-                .unwrap()
-                .to_str()
-                .unwrap()
-                .to_string();
-            let new_name = format!("_{}", file_stem);
-            let schema_path = path.to_str().unwrap();
-            println!("cargo:rerun-if-changed={}", schema_path);
-            Command::new(BEBOP_BIN)
-                .arg("--files")
-                .arg(schema_path)
-                .arg("--rust")
-                .arg(format!("./src/{}.rs", &new_name))
-                .output()
-                .expect("failed to build schema");
-            list.push_back(new_name);
-        } else {
-            // do nothing
-        }
+    unsafe {
+        bebop::COMPILER_PATH = Some(PathBuf::from(BEBOP_BIN));
+        bebop::GENERATED_PREFIX = Some("_".into());
     }
-    list
+    bebop::build_schema_dir("../../Schemas", "src/generated");
 }
--- Runtime/Rust/src/lib.rs
+++ Runtime/Rust/src/lib.rs
@@ -6,5 +6,9 @@ pub use types::*;
 
 pub mod prelude {
     pub use crate::serialization::Record;
+    pub use crate::SliceWrapper;
     pub use crate::types::{Date, Guid};
 }
+
+#[cfg(feature = "cargo")]
+pub mod cargo;
