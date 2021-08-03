89f1071a5a0f1be62fd4e78eaf49689657baa566
Evan Banyash <ebanyash@gmail.com>
2021-08-03 12:14:46 -0700

    Maybe that will do it.

--- Core/Meta/VersionInfo.cs
+++ Core/Meta/VersionInfo.cs
@@ -32,6 +32,6 @@ namespace Core.Meta
         /// <summary>
         /// The human readable version which may contain additional labels.
         /// </summary>
-        public const string Informational = "0.0.1-20210803-1903";
+        public const string Informational = "0.0.1-20210803-1914";
     }
 }
