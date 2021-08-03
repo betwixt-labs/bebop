92e8c3ca9f79ab3fbde6105623be9a61483fe1db
Evan Banyash <ebanyash@gmail.com>
2021-08-03 12:42:22 -0700

    Remove tests, revert sln.

--- Core/Meta/VersionInfo.cs
+++ Core/Meta/VersionInfo.cs
@@ -32,6 +32,6 @@ namespace Core.Meta
         /// <summary>
         /// The human readable version which may contain additional labels.
         /// </summary>
-        public const string Informational = "0.0.1-20210803-1914";
+        public const string Informational = "0.0.1-20210803-1933";
     }
 }
