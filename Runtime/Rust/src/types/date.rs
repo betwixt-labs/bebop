9601f14f2456f2cfc3a212c9ebe3e4dc79157c2a
Matthew Conover <matthew@rainway.com>
2021-08-09 16:01:49 -0700

    Correct date internal representation

--- Runtime/Rust/src/types/date.rs
+++ Runtime/Rust/src/types/date.rs
@@ -1,6 +1,9 @@
 use std::ops::{Deref, DerefMut};
 use std::time::Duration;
 
+/// The number of ticks between 1/1/0001 and 1/1/1970.
+const TICKS_BETWEEN_EPOCHS: u64 = 621355968000000000;
+
 /// A date is stored as a 64-bit integer amount of “ticks” since 00:00:00 UTC on January 1 of year
 /// 1 A.D. in the Gregorian calendar, where a “tick” is 100 nanoseconds.
 #[derive(Eq, PartialEq, Ord, PartialOrd, Hash, Clone, Copy, Debug)]
@@ -38,8 +41,18 @@ impl Date {
     }
 
     #[inline]
+    pub const fn from_ticks_since_unix_epoch(t: u64) -> Self {
+        Self(t + TICKS_BETWEEN_EPOCHS)
+    }
+
+    #[inline]
     pub const fn to_ticks(self) -> u64 {
         self.0
+    }
+
+    #[inline]
+    pub const fn to_ticks_since_unix_epoch(self) -> u64 {
+        self.0 - TICKS_BETWEEN_EPOCHS
     }
 
     #[inline]
@@ -48,8 +61,18 @@ impl Date {
     }
 
     #[inline]
+    pub const fn from_micros_since_unix_epoch(t: u64) -> Self {
+        Self::from_ticks_since_unix_epoch(t * 10)
+    }
+
+    #[inline]
     pub const fn to_micros(self) -> u64 {
         self.0 / 10
+    }
+
+    #[inline]
+    pub const fn to_micros_since_unix_epoch(self) -> u64 {
+        self.to_ticks_since_unix_epoch() / 10
     }
 
     #[inline]
@@ -58,8 +81,18 @@ impl Date {
     }
 
     #[inline]
+    pub const fn from_millis_since_unix_epoch(t: u64) -> Self {
+        Date::from_micros_since_unix_epoch(t * 1000)
+    }
+
+    #[inline]
     pub const fn to_millis(self) -> u64 {
         self.to_micros() / 1000
+    }
+
+    #[inline]
+    pub const fn to_millis_since_unix_epoch(self) -> u64 {
+        self.to_micros_since_unix_epoch() / 1000
     }
 
     #[inline]
@@ -68,8 +101,18 @@ impl Date {
     }
 
     #[inline]
+    pub const fn from_secs_since_unix_epoch(t: u64) -> Self {
+        Date::from_millis_since_unix_epoch(t * 1000)
+    }
+
+    #[inline]
     pub const fn to_secs(self) -> u64 {
         self.to_millis() / 1000
+    }
+
+    #[inline]
+    pub const fn to_secs_since_unix_epoch(self) -> u64 {
+        self.to_millis_since_unix_epoch() / 1000
     }
 
     #[inline]
@@ -78,12 +121,27 @@ impl Date {
     }
 
     #[inline]
+    pub fn to_micros_since_unix_epoch_f(self) -> f64 {
+        self.to_ticks_since_unix_epoch() as f64 / 10.
+    }
+
+    #[inline]
     pub fn to_millis_f(self) -> f64 {
         self.to_micros_f() / 1000.
     }
 
     #[inline]
+    pub fn to_millis_since_unix_epoch_f(self) -> f64 {
+        self.to_micros_since_unix_epoch_f() / 1000.
+    }
+
+    #[inline]
     pub fn to_secs_f(self) -> f64 {
         self.to_millis_f() / 1000.
+    }
+
+    #[inline]
+    pub fn to_secs_since_unix_epoch_f(self) -> f64 {
+        self.to_millis_since_unix_epoch_f() / 1000.
     }
 }
