bf237dddf474eac61bf63e2a9084b3430ed6b56c
Matthew Conover <matthew@rainway.com>
2021-08-12 08:28:40 -0700

    Integration tests CI

--- Core/Meta/VersionInfo.cs
+++ Core/Meta/VersionInfo.cs
@@ -32,6 +32,6 @@ namespace Core.Meta
         /// <summary>
         /// The human readable version which may contain additional labels.
         /// </summary>
-        public const string Informational = "0.0.1-20210812-1509";
+        public const string Informational = "0.0.1-20210812-1515";
     }
 }
