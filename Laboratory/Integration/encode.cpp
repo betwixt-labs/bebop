a4a8bd3212fc6ef7fbc48fadfbee000069ce37b6
Ger Hobbelt <ger@hobbelt.com>
2022-01-04 20:51:11 +0100

    integration test: make sure the encoder writes in BINARY MODE to stdout or the tests will all fail on MSWindows (which does implicit LF->CRLF conversion in the stdio streams).

--- Laboratory/Integration/encode.cpp
+++ Laboratory/Integration/encode.cpp
@@ -1,11 +1,18 @@
 #include <cstdio>
 #include <vector>
+#if defined(_WIN32) || defined(WIN32) || defined(WIN64)
+#include <fcntl.h>
+#include <io.h>
+#endif
 #include "makelib.hpp"
 
 int main()
 {
+  _setmode(_fileno(stdout), _O_BINARY);
+
   std::vector<uint8_t> buffer;
   Library::encodeInto(make_library(), buffer);
+
   std::fwrite(buffer.data(), sizeof(uint8_t), buffer.size(), stdout);
   return 0;
 }
