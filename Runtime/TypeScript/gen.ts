842b68884f738bc69b00f9ff8421e593c9470a55
Matthew Conover <matthew@rainway.com>
2022-03-15 15:52:47 -0700

    generation script

--- Runtime/TypeScript/gen.ts
+++ Runtime/TypeScript/gen.ts
@@ -1,17 +1,38 @@
-// dotnet run --project ../../Compiler --ts "generated/datagram.ts" --files ../../Core/Schemas/RpcDatagram.bop
+import { spawn } from "child_process";
+import * as fs from "fs/promises";
 
-async function buildSchema(source: string, dest: string): Promise<void> {}
+function buildSchema(source: string, dest: string): Promise<void> {
+  const child = spawn(
+    "dotnet",
+    ["run", "--project", "../../Compiler", "--ts", dest, "--files", source],
+    { stdio: ["ignore", "inherit", "inherit"] }
+  );
 
-async function replaceInFile(
-  path: string,
-  o: RegExp,
-  n: string
-): Promise<void> {}
+  return new Promise((resolve, reject) => {
+    child.once("close", resolve);
+    child.once("error", reject);
+  });
+}
 
 (async () => {
   await buildSchema(
     "../../Core/Schemas/RpcDatagram.bop",
-    "generated/datagram.ts"
+    "src/generated/datagram.ts.tmp"
   );
-  await replaceInFile("generated/datagram.ts", /from "bebop"/, 'from ".."');
+
+  // not the most efficient thing ever, but probably fine for compile time to
+  // just read all into memory, modify, and write all back out.
+
+  const gen = await fs.open("src/generated/datagram.ts.tmp", "r");
+  let data = await gen.readFile({ encoding: "utf8" });
+  gen.close().then(async () => {
+    await fs.rm("src/generated/datagram.ts.tmp");
+  });
+
+  data = data.replace('from "bebop"', 'from ".."');
+
+  await fs.rm("src/generated/datagram.ts", { force: true });
+  let out = await fs.open("src/generated/datagram.ts", "w");
+  await out.write(data);
+  await out.close();
 })();
