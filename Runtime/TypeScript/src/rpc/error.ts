853e6421f87c36fb39ddde942b110e594d344ffa
Matthew Conover <matthew@rainway.com>
2022-03-16 15:49:13 -0700

    Rpc errors

--- Runtime/TypeScript/src/rpc/error.ts
+++ Runtime/TypeScript/src/rpc/error.ts
@@ -38,3 +38,249 @@ export function set_on_respond_error(cb: OnRespondError): boolean {
     return false;
   }
 }
+
+export enum TransportErrorVariants {
+  DatagramTooLarge,
+  SerializationError,
+  DeserializationError,
+  NotConnected,
+  Timeout,
+  CallDropped,
+  Other,
+}
+
+/**
+ * The datagram is too large to send. Some transports may impose different limits, but Bebop
+ * itself has a 4GiB limit on the user data as it gets serialized into a byte array.
+ */
+export type TransportErrorDatagramTooLarge = {
+  readonly discriminator: TransportErrorVariants.DatagramTooLarge;
+};
+
+/** The serialization failed. */
+export type TransportErrorSerializationError = {
+  readonly discriminator: TransportErrorVariants.SerializationError;
+  readonly error: BebopRuntimeError;
+};
+
+/**
+ * Was unable to parse data received from the remote. This indicates an issue with the
+ * transport as signatures would catch non-agreeing structures.
+ */
+export type TransportErrorDeserializationError = {
+  readonly discriminator: TransportErrorVariants.DeserializationError;
+  readonly error: BebopRuntimeError;
+};
+
+/**
+ * The transport is not connected to the remote. Ideally the transport should handle this
+ * internally rather than returning this error unless there is no way for it to reconnect.
+ */
+export type TransportErrorNotConnected = {
+  readonly discriminator: TransportErrorVariants.NotConnected;
+};
+
+/** Provided timeout was reached before we received a response. */
+export type TransportErrorTimeout = {
+  readonly discriminator: TransportErrorVariants.Timeout;
+};
+
+/** The request handle was dropped and we no longer care about any received responses. */
+export type TransportErrorCallDropped = {
+  readonly discriminator: TransportErrorVariants.CallDropped;
+};
+
+/** A custom transport-specific error. */
+export type TransportErrorOther = {
+  readonly discriminator: TransportErrorVariants.Other;
+  readonly message: string;
+};
+
+export type TransportErrorInner =
+  | TransportErrorDatagramTooLarge
+  | TransportErrorSerializationError
+  | TransportErrorDeserializationError
+  | TransportErrorNotConnected
+  | TransportErrorTimeout
+  | TransportErrorCallDropped
+  | TransportErrorOther;
+
+/**
+ * Things that could go wrong with the underlying transport, need it to be somewhat generic.
+ * Things like the internet connection dying would fall under this.
+ */
+export class TransportError extends BebopRuntimeError {
+  constructor(public readonly inner: TransportErrorInner) {
+    switch (inner.discriminator) {
+      case TransportErrorVariants.DatagramTooLarge:
+        super("Datagram too large");
+        break;
+      case TransportErrorVariants.SerializationError:
+        super(`Serialization error: ${inner.error.message}`);
+        break;
+      case TransportErrorVariants.DeserializationError:
+        super(`Deserialization error: ${inner.error.message}`);
+        break;
+      case TransportErrorVariants.NotConnected:
+        super("Not connected");
+        break;
+      case TransportErrorVariants.Timeout:
+        super("Timeout");
+        break;
+      case TransportErrorVariants.CallDropped:
+        super("Call dropped");
+        break;
+      case TransportErrorVariants.Other:
+        super(inner.message);
+        break;
+      default:
+        super("Unknown transport error");
+    }
+  }
+}
+
+export enum LocalRpcErrorVariants {
+  Custom,
+  DeadlineExceeded,
+  NotSupported,
+}
+
+/**
+ * Custom error code with a message. You should use an Enum to keep the error codes synchronized.
+ */
+export type LocalRpcErrorCustom = {
+  readonly discriminator: LocalRpcErrorVariants.Custom;
+  readonly code: number;
+  readonly message: string;
+};
+
+/**
+ * This may be returned if the deadline provided to the function has been exceeded, however, it
+ * is also acceptable to return any other error OR okay value instead and a best-effort will be
+ * made to return it.
+ *
+ * Warning: Do not return this if there was no deadline provided to the function.
+ */
+export type LocalRpcErrorDeadlineExceeded = {
+  readonly discriminator: LocalRpcErrorVariants.DeadlineExceeded;
+};
+
+/**
+ * Indicates a given operation has not been implemented for the given service.
+ */
+export type LocalRpcErrorNotSupported = {
+  readonly discriminator: LocalRpcErrorVariants.NotSupported;
+};
+
+export type LocalRpcErrorInner =
+  | LocalRpcErrorCustom
+  | LocalRpcErrorDeadlineExceeded
+  | LocalRpcErrorNotSupported;
+
+/** Errors that the local may return when sending or responding to a request. */
+export class LocalRpcError extends BebopRuntimeError {
+  constructor(public readonly inner: LocalRpcErrorInner) {
+    switch (inner.discriminator) {
+      case LocalRpcErrorVariants.DeadlineExceeded:
+        super("Deadline exceeded");
+        break;
+      case LocalRpcErrorVariants.Custom:
+        super(`Custom (${inner.code}) ${inner.message}`);
+        break;
+      case LocalRpcErrorVariants.NotSupported:
+        super("Not supported");
+        break;
+      default:
+        super("Unknown local rpc error");
+    }
+  }
+}
+
+export enum RemoteRpcErrorVariants {
+  Transport,
+  Custom,
+  UnknownCall,
+  InvalidSignature,
+  CallNotSupported,
+  RemoteDecode,
+}
+
+/** A transport error occurred whilst waiting for a response. */
+export type RemoteRpcErrorTransport = {
+  readonly discriminator: RemoteRpcErrorVariants.Transport;
+  readonly error: TransportError;
+};
+
+/**
+ * The remote generated a custom error message. You should use an Enum to keep the error codes
+ * synchronized.
+ */
+export type RemoteRpcErrorCustom = {
+  readonly discriminator: RemoteRpcErrorVariants.Custom;
+  readonly code: number;
+  readonly message?: string;
+};
+
+/**
+ * The remote did not recognize the opcode. This means the remote either dropped support for
+ * this call or the remote is outdated.
+ */
+export type RemoteRpcErrorUnknownCall = {
+  readonly discriminator: RemoteRpcErrorVariants.UnknownCall;
+};
+
+/**
+ * Our call signature did not match that of the remote which means there is probably a version
+ * mismatch or possibly they are exposing a different service than expected.
+ */
+export type RemoteRpcErrorInvalidSignature = {
+  readonly discriminator: RemoteRpcErrorVariants.InvalidSignature;
+  readonly expected: number;
+};
+
+/** There is no implementation for this call on the remote at this time. */
+export type RemoteRpcErrorCallNotSupported = {
+  readonly discriminator: RemoteRpcErrorVariants.CallNotSupported;
+};
+
+/** Remote was not able to decode our message. */
+export type RemoteRpcErrorRemoteDecode = {
+  readonly discriminator: RemoteRpcErrorVariants.RemoteDecode;
+  readonly message?: string;
+};
+
+export type RemoteRpcErrorInner =
+  | RemoteRpcErrorTransport
+  | RemoteRpcErrorCustom
+  | RemoteRpcErrorUnknownCall
+  | RemoteRpcErrorInvalidSignature
+  | RemoteRpcErrorCallNotSupported
+  | RemoteRpcErrorRemoteDecode;
+
+/** Errors that can be received from the remote when making a request. */
+export class RemoteRpcError extends BebopRuntimeError {
+  constructor(public readonly inner: RemoteRpcErrorInner) {
+    switch (inner.discriminator) {
+      case RemoteRpcErrorVariants.UnknownCall:
+        super("Unknown call");
+        break;
+      case RemoteRpcErrorVariants.Transport:
+        super(inner.error.message);
+        break;
+      case RemoteRpcErrorVariants.Custom:
+        super(`Custom (${inner.code}) ${inner.message}`);
+        break;
+      case RemoteRpcErrorVariants.InvalidSignature:
+        super("Invalid signature");
+        break;
+      case RemoteRpcErrorVariants.CallNotSupported:
+        super("Call not supported");
+        break;
+      case RemoteRpcErrorVariants.RemoteDecode:
+        super(`Remote decode ${inner.message}`);
+        break;
+      default:
+        super("Unknown remote rpc error");
+    }
+  }
+}
