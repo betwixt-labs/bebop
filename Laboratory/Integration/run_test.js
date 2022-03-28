8c68b6e896fd68aa31e2dc2542644c21269266e0
Lynn <lynn@foldr.moe>
2022-03-29 01:59:26 +0200

    Fixes for Windows

--- Laboratory/Integration/run_test.js
+++ Laboratory/Integration/run_test.js
@@ -1,5 +1,6 @@
 const shell = require("shelljs");
 const cxx = shell.env["CXX"] || "g++";
+const aout = process.platform === "win32" ? "a.exe" : "./a.out";
 
 shell.echo("Compiling schema...");
 if (shell.exec("dotnet run --project ../../Compiler --files schema.bop --cs schema.cs --ts schema.ts --cpp schema.hpp --rust Rust/src/schema.rs").code !== 0) {
@@ -7,19 +8,16 @@ if (shell.exec("dotnet run --project ../../Compiler --files schema.bop --cs sche
   shell.exit(1);
 }
 
-shell.echo("Running npm install...");
-shell.exec("npm install");
-
 shell.echo("Generating encodings from each language...")
-shell.exec("dotnet run encode").to("cs.enc");
-shell.exec("npx ts-node encode.ts").to("ts.enc");
+shell.exec("dotnet run encode > cs.enc");
+shell.exec("npx ts-node encode.ts > ts.enc");
 if (shell.exec(`${cxx} --std=c++17 encode.cpp`).code !== 0) {
   shell.echo(`Error: ${cxx} encode.cpp failed`);
   shell.exit(1);
 }
-shell.exec("./a.out").to("cpp.enc");
-shell.rm("-f", "./a.out");
-shell.exec("cargo run --manifest-path Rust/Cargo.toml --example encode").to("rs.enc");
+shell.exec(`${aout} > cpp.enc`);
+shell.rm("-f", aout);
+shell.exec("cargo run --manifest-path Rust/Cargo.toml --example encode > rs.enc");
 
 // Files can have some variance and still be equivalent because of ordering in maps and C# dates.
 // Perform full matrix testing because it seems like a good idea
@@ -35,12 +33,13 @@ const languages = [
   {name: "Rust", cmd: "cargo run --manifest-path Rust/Cargo.toml -q --example decode --", file: "rs.enc"},
   {name: "C#", cmd: "dotnet run decode", file: "cs.enc"},
   {name: "TypeScript", cmd: "npx ts-node decode.ts", file: "ts.enc"},
-  {name: "C++", cmd: "./a.out", file: "cpp.enc"},
+  {name: "C++", cmd: aout, file: "cpp.enc"},
 ];
 
 var failed = false;
 for (const decode of languages) {
   for (const encode of languages) {
+    shell.echo(`\x1b[33m${decode.cmd} ${encode.file}\x1b[0m`);
     if (shell.exec(`${decode.cmd} ${encode.file}`).code !== 0) {
       shell.echo(`\x1b[31mError: ${decode.name} failed to decode ${encode.name}\x1b[0m`);
       failed = true;
@@ -48,7 +47,7 @@ for (const decode of languages) {
   }
 }
 
-shell.rm("-f", "./a.out");
+shell.rm("-f", aout);
 
 if (failed) {
   shell.exit(1);
