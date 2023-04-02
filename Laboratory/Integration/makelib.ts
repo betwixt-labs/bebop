487e892b426fce7d7da2170078521cdc9a67881a
andrew <hi@andrew.im>
2023-04-02 03:05:14 -0400

    More test fixes

--- Laboratory/Integration/makelib.ts
+++ Laboratory/Integration/makelib.ts
@@ -1,52 +1,47 @@
-﻿import {Library, ILibrary, Instrument} from "./schema"
+﻿import { Library, ILibrary, Instrument, Album } from "./schema"
 
-export function makelib(): ILibrary {
-    return {
+export function makelib(): Library {
+    return new Library({
         albums: new Map([
-            ["Giant Steps", {
-                discriminator: 1, value: {
-                    tracks: [
-                        {
-                            title: "Giant Steps",
-                            year: 1959,
-                            performers: [{name: "John Coltrane", plays: Instrument.Piano}],
-                        }, {
-                            title: "A Night in Tunisia",
-                            year: 1942,
-                            performers: [
-                                {name: "Dizzy Gillespie", plays: Instrument.Trumpet},
-                                {name: "Count Basie", plays: Instrument.Piano},
-                            ]
-                        }, {
-                            title: "Groovin' High"
-                        }
-                    ]
-                }
-            }],
-            ["Adam's Apple", {
-                discriminator: 2, value: {
-                    venueName: "Tunisia",
-                    concertDate: new Date(528205479000)
-                }
-            }],
-            ["Milestones", {
-                discriminator: 1, value: {
-                    tracks: []
-                }
-            }],
-            ["Brilliant Corners", {
-                discriminator: 2, value: {
-                    venueName: "Night's Palace",
-                    tracks: [{
-                        year: 1965,
+            ["Giant Steps", Album.fromStudioAlbum({
+                tracks: [
+                    {
+                        title: "Giant Steps",
+                        year: 1959,
+                        performers: [{ name: "John Coltrane", plays: Instrument.Piano }],
+                    }, {
+                        title: "A Night in Tunisia",
+                        year: 1942,
                         performers: [
-                            {name: "Carmell Jones", plays: Instrument.Trumpet},
-                            {name: "Joe Henderson", plays: Instrument.Sax},
-                            {name: "Teddy Smith", plays: Instrument.Clarinet}
+                            { name: "Dizzy Gillespie", plays: Instrument.Trumpet },
+                            { name: "Count Basie", plays: Instrument.Piano },
                         ]
-                    }]
-                }
-            }],
+                    }, {
+                        title: "Groovin' High"
+                    }
+                ]
+            })],
+            ["Adam's Apple", Album.fromLiveAlbum({
+                venueName: "Tunisia",
+                concertDate: new Date(528205479000)
+            })
+            ],
+            ["Milestones", Album.fromStudioAlbum({
+                tracks: []
+            })
+            ],
+            ["Brilliant Corners", Album.fromLiveAlbum({
+                venueName: "Night's Palace",
+                tracks: [{
+                    year: 1965,
+                    performers: [
+                        { name: "Carmell Jones", plays: Instrument.Trumpet },
+                        { name: "Joe Henderson", plays: Instrument.Sax },
+                        { name: "Teddy Smith", plays: Instrument.Clarinet }
+                    ]
+                }]
+            })
+            ],
         ])
-    }
+    });
 }
