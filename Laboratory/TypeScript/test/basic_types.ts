f27028169dd01300970246be649d5a4b6445a504
andrew <hi@andrew.im>
2023-04-07 02:09:45 -0400

    add test

--- Laboratory/TypeScript/test/basic_types.ts
+++ Laboratory/TypeScript/test/basic_types.ts
@@ -1,5 +1,5 @@
 import { BebopView } from 'bebop';
-import { BasicTypes } from './generated/gen';
+import { BasicTypes, U } from './generated/gen';
 import * as assert from "assert";
 
 it("Basic types roundtrip", () => {
@@ -21,4 +21,27 @@ it("Basic types roundtrip", () => {
     const bytes = BasicTypes.encode(obj);
     const obj2 = BasicTypes.decode(bytes);
     expect(obj).toEqual(obj2);
+});
+
+it("Allows multiple encoding calls", () => {
+    const obj = {
+        a_bool: true,
+        a_byte: 1,
+        a_int16: 2,
+        a_uint16: 3,
+        a_int32: 4,
+        a_uint32: 5,
+        a_int64: BigInt(6),
+        a_uint64: BigInt(7),
+        a_float32: 8,
+        a_float64: 9,
+        a_string: 'hello world',
+        a_guid: '01234567-0123-0123-0123-0123456789ab',
+        a_date: new Date(1996, 1, 7),
+    };
+    const firstBytes = BasicTypes.encode(obj);
+    const union = U.fromA({ b: 12345 });
+    const unionBytes = U.encode(union);
+    expect(obj).toEqual(BasicTypes.decode(firstBytes));
+    expect(union).toEqual(U.decode(unionBytes));
 });
