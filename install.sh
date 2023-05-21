9af5fa0c462e68abc456d709b7a85d7850bd3d5f
AndrewMD5 <andrew@rainway.com>
2023-05-21 13:19:48 +0000

    Deploying to gh-pages from @ betwixt-labs/bebop@714550a38c11548b135db9acfe369f9c1f5e2c87 🚀

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ if [ -z "${BASH_VERSION:-}" ]; then
 fi
 
 ### Constants
-readonly BEBOPC_VERSION="${1:-2.6.3}"
+readonly BEBOPC_VERSION="${1:-2.6.4}"
 readonly BEBOP_RELEASE_URL="https://api.github.com/repos/betwixt-labs/bebop/releases/tags/v${BEBOPC_VERSION}"
 
 ### string formatters
