bbe84d6add1a3589a5f22867f3149597d59183ce
andrewmd5 <1297077+andrewmd5@users.noreply.github.com>
2024-08-09 05:04:08 +0000

    Deploying to gh-pages from @ betwixt-labs/bebop@1a78cd6bba62e362015c7dd8f2d6c9bbba1af5b8 🚀

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 param (
     [Parameter(HelpMessage = 'The version of bebopc to install')]
-    [string]$bebopcVersion = '3.0.12',
+    [string]$bebopcVersion = '3.0.13',
     [Parameter(HelpMessage = 'The URL where bebpc artifacts will be fetched from')]
     [string]$manifestUrl = "https://api.github.com/repos/betwixt-labs/bebop/releases/tags/v${bebopcVersion}"
 )
